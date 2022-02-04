cc3588cc9c9eea0952314c7cacf415b8fda50516
Rik-Fox <richard.fox@warwick.ac.uk>
2022-02-04 14:25:17 +0000

    removed history dir

--- .history/random_choices_20220202152414.py
+++
@@ -1,9 +0,0 @@
-import random
-import matplotlib.pyplot as plt
-
-x = [random.random.rand() for i in range(5)]
-
-samp = [random.choices(x, weights=x) for i in range(100)]
-print(x)
-plt.hist(samp)
-plt.show()
